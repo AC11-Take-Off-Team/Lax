a5b9a7df9c8e839b32a8d05fa3d30914e5d37e04
dylan <dyan2009200@gmail.com>
2022-08-20 11:47:29 +0800

    drop_table users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_17_095428) do
+ActiveRecord::Schema.define(version: 2022_08_20_034524) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
