f9492d6b7e7552f6e53b77ffe7bbcff4fa65cbd6
Aila Lu <ailalutw@gmail.com>
2022-08-23 19:51:57 +0800

    message rendered! but channel not connected

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,18 +1,39 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1}, {
-  connected() {
-    console.log("connected!");// Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', () => {
+  const room_element = document.getElementById('room-id');
+  const room_id = room_element.getAttribute('data-room-id');
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+  console.log(consumer.subscriptions)
 
-  received(data) {
-    console.log(data);
-    const = messageContainer = document.getElementById('messages')
-    messageContainer.innerHTML = messageContainer.innerHTML + data.html
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+  consumer.subscriptions.subscriptions.forEach((subscription) => {
+    consumer.subscriptions.remove(subscription)
+  })
+
+  consumer.subscriptions.create({ channel: "RoomChannel", room_id: room_id }, {
+    connected() {
+      console.log("connected to " + room_id)
+      // Called when the subscription is ready for use on the server
+    },
+
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
+
+    received(data) {
+      const user_element = document.getElementById('user-id');
+      const user_id = Number(user_element.getAttribute('data-user-id'));
+
+      let html;
+
+      if (user_id === data.message.user_id) {
+        html = data.mine
+      } else {
+        html = data.theirs
+      }
+
+      const messageContainer = document.getElementById('messages')
+      messageContainer.innerHTML = messageContainer.innerHTML + html
+    }
+  });
+})
--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -2,11 +2,16 @@ class SendMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    html = ApplicationController.render(
-      partial: 'messages/message',
+    my_message = ApplicationController.render(
+      partial: 'messages/my_message',
       locals: {message: message}
   )
+
+  their_message = ApplicationController.render(
+    partial: 'messages/their_message',
+    locals: {message: message}
+)
   
-  ActionCable.server.broadcast "room_channel_#{message.room_id}", html: html
+  ActionCable.server.broadcast "room_channel_#{message.room_id}", my_message: my_message, their_message: their_message, message: message
   end
 end
