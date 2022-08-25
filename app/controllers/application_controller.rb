105f3851e44d477537e23b6d115ee6cdef534a4d
kathy <feather080300@gmail.com>
2022-08-25 18:18:58 +0800

    add search

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :search
+
+
+
+  def search
+    @q = Group.ransack(params[:q])
+  end
 end
---
+++ db/migrate/20220825100250_create_channels.rb
@@ -0,0 +1,10 @@
+class CreateChannels < ActiveRecord::Migration[6.1]
+  def change
+    create_table :channels do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :group, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
