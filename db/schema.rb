9d862a65e1da2aa8e00398c075470f7620c51e79
kathy <feather080300@gmail.com>
2022-08-30 12:10:15 +0800

    修改

--- db/schema.rb
+++ db/schema.rb
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_160027) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+
     t.string "nickname"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
@@ -88,6 +89,17 @@ ActiveRecord::Schema.define(version: 2022_08_29_160027) do
     t.string "google_uid"
     t.string "google_token"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+
+    t.string "nickname"
+    t.string "invitation_token"
+    t.datetime "invitation_created_at"
+    t.datetime "invitation_sent_at"
+    t.datetime "invitation_accepted_at"
+    t.integer "invitation_limit"
+    t.string "invited_by_type"
+    t.bigint "invited_by_id"
+    t.integer "invitations_count", default: 0
+
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
