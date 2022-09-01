7141a29c31288f6501b721dfb69b11e58d51a23a
kathy <feather080300@gmail.com>
2022-09-01 21:51:15 +0800

    首頁

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "../application.css";
+import "stylesheets";
 
 Rails.start();
 Turbolinks.start();
