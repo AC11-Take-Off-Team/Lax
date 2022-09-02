f85be40ce58d2ac8f92e9d290e29c913b2575354
Ning <a24701770@gmail.com>
2022-09-02 18:56:10 +0800

    add FavoriteList expand & collapse

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,15 +1,15 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["navBar", "navBtn"];
+  static targets = ["navBar", "navBtn", "favList"];
 
   // Navbar 開 & 收
   setNav() {
-    let status = JSON.parse(localStorage.getItem("isClick"));
+    let navStatus = JSON.parse(localStorage.getItem("NavClicked?"));
 
-    if (status == true) {
+    if (navStatus == true) {
       this.navBarTarget.style.display = "block";
-      localStorage.setItem("isClick", false);
+      localStorage.setItem("NavClicked?", false);
       this.navBtnTarget.style.position = "absolute";
       this.navBtnTarget.style.left = "80%";
       this.navBtnTarget.style.top = "0.75rem";
@@ -18,13 +18,20 @@ export default class extends Controller {
       this.navBtnTarget.style.position = "fixed";
       this.navBtnTarget.style.left = "0";
       this.navBtnTarget.style.top = "5rem";
-      localStorage.setItem("isClick", true);
+      localStorage.setItem("NavClicked?", true);
     }
   }
 
   // Favorite 開 & 收
   setFav() {
-    const favoriteList = document.querySelector("nav[data-id=favorite] div");
-    favoriteList.style.display = "none";
+    let favStatus = JSON.parse(localStorage.getItem("favClicked?"));
+
+    if (favStatus == true) {
+      this.favListTarget.style.display = "none";
+      localStorage.setItem("favClicked?", false);
+    } else {
+      this.favListTarget.style.display = "block";
+      localStorage.setItem("favClicked?", true);
+    }
   }
 }
