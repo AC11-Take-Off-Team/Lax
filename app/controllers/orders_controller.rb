70fdcedaa691ddbc8f07c1ec8646b5000166e04d
dylan <dyan2009200@gmail.com>
2022-09-04 16:51:53 +0800

    增加狀態機到order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
+  before_action : :authenticate_user!
   def new
     @order = Order.new
+  end
+
+  def create
+    render html: params
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,28 @@
 class Order < ApplicationRecord
+  include AASM
   belongs_to :user
 
   validates :serial, :price, :state, presence: true
+
+  aasm column: "state", no_direct_assignment: true do 
+    state :pending, initial: true
+    state :paid, :refunded, :cancelled, :fail
+
+    event :pay do 
+      transitions from: [:panding, :fail], to: :paid
+    end
+
+    event :fail do
+      transitions from: :panding, to: :fail
+    end
+
+    event :cancel do 
+      transitions from: [:panding, :fail], to: :cancelled
+    end
+
+    event :refund do 
+      transitions from: :paid, to: :refunded
+    end
+  end
+
 end
