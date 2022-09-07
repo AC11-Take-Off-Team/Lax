8cf9d74af5b2b641b5a3c0d6acad376c8bcbbdff
kathy <feather080300@gmail.com>
2022-09-07 17:30:34 +0800

    修排版

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "../application.css";
-import "stylesheets";
+// import "stylesheet";
 
 Rails.start();
 Turbolinks.start();
