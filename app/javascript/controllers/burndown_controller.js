d1a342a1d9cef7314a3f60557a84e1c52c02acde
Ning <a24701770@gmail.com>
2022-09-11 13:32:02 +0800

    add basic burndown

--- app/javascript/controllers/burndown_controller.js
+++ app/javascript/controllers/burndown_controller.js
@@ -1,36 +1,69 @@
 import { Controller } from "stimulus";
-import { Chart, registerables } from 'chart.js';
+import { Chart, registerables } from "chart.js";
 Chart.register(...registerables);
-import { getRelativePosition } from 'chart.js/helpers';
+import { getRelativePosition } from "chart.js/helpers";
 import Rails from "@rails/ujs";
 
-
 export default class extends Controller {
-  static targets = ["chart"]
+  static targets = ["chart"];
 
   initialize() {
-    this.chartArea = this.chartTarget.getContext('2d')
+    this.projectId = 0;
+    this.chartArea = this.chartTarget.getContext("2d");
+    this.taskCount = 0;
+    this.taskStart = null;
+    this.taskEnd = null;
   }
 
   connect() {
-    this.createChart()
-    console.log(this.chartArea);
+    this.projectId = this.element.dataset.projectId;
+    this.taskCount = this.element.dataset.taskCount;
+    this.taskStart = this.element.dataset.taskStart;
+    this.taskEnd = this.element.dataset.taskEnd;
+    console.log(this.taskCount, this.taskStart);
+
+    this.createChart();
+    this.fetchProject();
+  }
+
+  fetchProject() {
+    Rails.ajax({
+      url: `/projects/${this.projectId}/tasks`,
+      type: "GET",
+      success: () => {},
+      error: () => {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+          footer: '<a href="/">Back to Home</a>'
+        });
+      }
+    });
   }
 
   createChart() {
     new Chart(this.chartArea, {
-      type: 'line',
+      type: "scatter",
       data: {
-        labels: ['1', '2', '3', '4'],
-        datasets: [{
-          label: '',
-          backgroundColor: 'rgb(255, 99, 132)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [0, 10, 5, 2, 20, 30, 45],
-        }]
+        labels: ["Day1", "Day2", "Day3", "Day4"],
+        datasets: [
+          {
+            type: "line",
+            label: "實際進度",
+            data: [0.9, 0.8],
+            fill: false,
+            borderColor: "rgb(54, 162, 235)"
+          },
+          {
+            type: "line",
+            label: "預期進度",
+            data: [1, 0.75, 0.5, 0.25, 0],
+            fill: false,
+            borderColor: "rgb(54, 162, 235)"
+          }
+        ]
       }
-    })
+    });
   }
 }
-
-
