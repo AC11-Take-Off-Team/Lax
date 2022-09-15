056b757ecc9b27647eb4e0302ed851799245ccb7
Ning <a24701770@gmail.com>
2022-09-16 01:52:24 +0800

    add create project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @projects = current_user.projects.count
+
+    @projects = current_user.projects
+    @first_project = @projects.first
+    @groups = current_user.groups
+    # 撈代辦
+    # undo_list = Columns.where(status: "待辦事項").where(project_id: @first_project.id)
+    # @undo_tasks = undo_list
   end
 end
