ed291b4edcd95c841b5c9cbc4946059b3dae73a9
Ning <a24701770@gmail.com>
2022-09-19 00:00:28 +0800

    add message view

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,12 +1,14 @@
-document.addEventListener('turbolinks:load', () => {
-  const room_element = document.getElementById('room-id');
+import consumer from "./consumer";
+
+document.addEventListener("turbolinks:load", () => {
+  const room_element = document.getElementById("room-id");
   if (room_element != null) {
-    const room_id = room_element.getAttribute('data-room-id');
+    const room_id = room_element.getAttribute("data-room-id");
     consumer.subscriptions.create(
-      { channel: 'RoomChannel', room_id: room_id },
+      { channel: "RoomChannel", room_id: room_id },
       {
         connected() {
-          console.log('connected to room' + room_id);
+          console.log("connected to room" + room_id);
           // Called when the subscription is ready for use on the server
         },
 
@@ -15,8 +17,8 @@ document.addEventListener('turbolinks:load', () => {
         },
 
         received(data) {
-          const user_element = document.getElementById('user-id');
-          const user_id = user_element.getAttribute('data-user-id');
+          const user_element = document.getElementById("user-id");
+          const user_id = user_element.getAttribute("data-user-id");
 
           let html;
 
@@ -26,12 +28,12 @@ document.addEventListener('turbolinks:load', () => {
             html = data.their_message;
           }
 
-          const messageContainer = document.getElementById('messages');
+          const messageContainer = document.getElementById("messages");
           messageContainer.innerHTML = messageContainer.innerHTML + html;
-          const message_value = document.querySelector('#message_content')
-          message_value.value = ''
-        },
+          const message_value = document.querySelector("#message_content");
+          message_value.value = "";
+        }
       }
     );
   }
-})
+});
