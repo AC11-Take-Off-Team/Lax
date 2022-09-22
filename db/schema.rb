e7ab7dbda5625cbbadd5ab564480ae4cc720ffaa
kathy <feather080300@gmail.com>
2022-09-23 00:52:30 +0800

    add_about

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2022_09_20_171946) do
+ActiveRecord::Schema.define(version: 2022_09_19_160908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,8 +89,6 @@ ActiveRecord::Schema.define(version: 2022_09_20_171946) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "owner_id"
-    t.datetime "start_time"
-    t.datetime "end_time"
     t.index ["deleted_at"], name: "index_projects_on_deleted_at"
   end
 
