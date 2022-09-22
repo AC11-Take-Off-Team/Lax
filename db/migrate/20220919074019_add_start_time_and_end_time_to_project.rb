1d5d76011616a48b23b2bb49f194412ff70fb5fc
kathy <feather080300@gmail.com>
2022-09-22 15:29:40 +0800

    dev

--- db/migrate/20220919074019_add_start_time_and_end_time_to_project.rb
+++
@@ -1,6 +0,0 @@
-class AddStartTimeAndEndTimeToProject < ActiveRecord::Migration[6.1]
-  def change
-    add_column :projects, :start_time, :datetime
-    add_column :projects, :end_time, :datetime
-  end
-end
