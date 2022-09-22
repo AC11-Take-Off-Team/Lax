c1e691bbad832da770f9a215335e438886cbf68b
Ning <a24701770@gmail.com>
2022-09-22 22:34:46 +0800

    fix borad

--- app/javascript/controllers/task_controller.js
+++ app/javascript/controllers/task_controller.js
@@ -1,18 +1,18 @@
-import { Controller } from 'stimulus';
-import picker from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import Rails from '@rails/ujs';
-import Swal from 'sweetalert2';
+import { Controller } from "stimulus";
+import picker from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Rails from "@rails/ujs";
+import Swal from "sweetalert2";
 
 export default class extends Controller {
-  static targets = ['datepicker', 'task_display', 'column_status'];
+  static targets = ["datepicker", "task_display", "column_status", "card"];
 
   initialize() {
     this.datePicker = null;
   }
   connect() {
     this.datePicker = picker(this.datepickerTargets, {
-      mode: 'range',
+      mode: "range"
     }); //使this.element變成月曆畫面
   }
   disconnect() {
@@ -20,35 +20,36 @@ export default class extends Controller {
   }
   display_change() {
     const display = this.task_displayTarget;
-    display.style.display == 'block' ? (display.style.display = 'none') : (display.style.display = 'block');
+    display.style.display == "block"
+      ? (display.style.display = "none")
+      : (display.style.display = "block");
   }
   selectdate() {
     const start_time = this.datePicker.selectedDates[0];
     const end_time = this.datePicker.selectedDates[1];
     const data = new FormData();
-    data.append('start_time', start_time);
+    data.append("start_time", start_time);
     if (end_time !== undefined) {
-      data.append('end_time', end_time);
+      data.append("end_time", end_time);
     }
   }
   taskPost() {
     const projectID = this.element.dataset.projectId;
     Rails.ajax({
       url: `/projects/${projectID}/tasks?status=${aaa}`,
-      type: 'post',
-      success: () => {
-      },
+      type: "post",
+      success: () => {},
       error: (err) => {
-        Swal.fire('日期指定失敗');
-      },
+        Swal.fire("日期指定失敗");
+      }
     });
-    this.task_displayTarget.style.display = 'none';
+    this.task_displayTarget.style.display = "none";
   }
   closePost(event) {
     event.preventDefault();
-    const cross = event.currentTarget.closest(".task_form")
+    const cross = event.currentTarget.closest(".task_form");
 
-    cross.style.display = 'none';
-    this.task_displayTarget.style.display = 'none';
+    cross.style.display = "none";
+    this.task_displayTarget.style.display = "none";
   }
 }
